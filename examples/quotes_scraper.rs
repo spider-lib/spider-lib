8659bcf5c62fc66c99f570b62da4998708e85134
mzyui <xnver404@gmail.com>
2026-02-08 23:18:30 +0700

    refactor(core): update spider-lib and example to new api
    Updates `spider-core` to `0.1.2` and other internal crates to use local path dependencies.
    Rewrites the `quotes_scraper` example to demonstrate the new `Spider` trait and `CrawlerBuilder`.

--- examples/quotes_scraper.rs
+++ examples/quotes_scraper.rs
@@ -1,19 +1,61 @@
-use spider_lib::{ScrapedItem, SpiderError};
-use spider_macro::scraped_item;
+// Use the prelude for easy access to common types and traits.
+use spider_lib::prelude::*;
 
 #[scraped_item]
-struct QuoteItem {
-    text: String,
-    author: String,
-    author_link: String,
+pub struct QuoteItem {
+    pub text: String,
+    pub author: String,
 }
 
+pub struct QuotesSpider;
 
-fn main() -> Result<(), SpiderError> {
-    // Initialize tracing with the specified configuration
-    tracing_subscriber::fmt()
-        .with_env_filter("info,spider_lib=debug")
-        .init();
-    
+#[async_trait]
+impl Spider for QuotesSpider {
+    type Item = QuoteItem;
+
+    fn start_urls(&self) -> Vec<&'static str> {
+        vec!["http://quotes.toscrape.com/"]
+    }
+
+    async fn parse(&mut self, response: Response) -> Result<ParseOutput<Self::Item>, SpiderError> {
+        let html = response.to_html()?;
+        let mut output = ParseOutput::new();
+
+        for quote in html.select(&".quote".to_selector()?) {
+            let text = quote
+                .select(&".text".to_selector()?)
+                .next()
+                .map(|e| e.text().collect())
+                .unwrap_or_default();
+            let author = quote
+                .select(&".author".to_selector()?)
+                .next()
+                .map(|e| e.text().collect())
+                .unwrap_or_default();
+            output.add_item(QuoteItem { text, author });
+        }
+
+        if let Some(next_href) = html
+            .select(&".next > a[href]".to_selector()?)
+            .next()
+            .and_then(|a| a.attr("href"))
+        {
+            let next_url = response.url.join(next_href)?;
+            output.add_request(Request::new(next_url));
+        }
+
+        Ok(output)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), SpiderError> {
+    tracing_subscriber::fmt().with_env_filter("info").init();
+
+    // The builder defaults to using ReqwestClientDownloader
+    let crawler = CrawlerBuilder::new(QuotesSpider).build().await?;
+
+    crawler.start_crawl().await?;
+
     Ok(())
 }
